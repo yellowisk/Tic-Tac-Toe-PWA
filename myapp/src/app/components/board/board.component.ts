089e31a7b2df554ecbcd13cbb49c92de53e79af7
yellowisk <heitor.a@aluno.ifsp.edu.br>
2023-07-24 20:09:28 -0300

    feat: click to make move and new possible moves for winning

--- myapp/src/app/components/board/board.component.ts
+++ myapp/src/app/components/board/board.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
-export class BoardComponent {
+export class BoardComponent implements OnInit {
   squares!: any[];
   xIsNext!: boolean;
   winner!: string | null;
@@ -28,21 +28,27 @@ export class BoardComponent {
 
   makeMove(idx: number) {
     if (!this.squares[idx]) {
-      this.squares.splice(idx, 1, this.player)
-      this.xIsNext = !this.xIsNext
+      this.squares.splice(idx, 1, this.player);
+      this.xIsNext = !this.xIsNext;
     }
+
+    this.winner = this.calculateWinner();
+    console.log(this.player + " made a move at " + idx);
+
   }
 
   calculateWinner() {
     const lines = [
       [0, 1, 7],
+      [0, 1, 2],
       [3, 4, 5],
       [6, 7, 8],
       [0, 3, 6],
       [1, 4, 7],
       [2, 5, 8],
       [0, 4, 8],
-      [2, 4, 6]
+      [2, 4, 6],
+      [7, 8, 9]
     ];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
